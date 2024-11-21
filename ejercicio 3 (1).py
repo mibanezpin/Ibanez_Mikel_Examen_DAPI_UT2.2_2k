68d5ab5000690ce43cf3b9c9a7add55abd038447
mibanezpin <mibanezpin@educacion.navarra.es>
2024-11-21 20:21:17 +0100

    ejercicio 3

---
+++ ejercicio 3 (1).py
@@ -0,0 +1,9 @@
+palabra= input("Introduce una palabra")
+consonante= input("Introduce una consonante")
+vocal= input("Introduce una vocal")
+consonantemayuscula=consonante.upper()
+consonanteminuscula=consonante.lower() 
+palabra=palabra.replace(consonanteminuscula,consonantemayuscula,)
+print(palabra)
+
+
