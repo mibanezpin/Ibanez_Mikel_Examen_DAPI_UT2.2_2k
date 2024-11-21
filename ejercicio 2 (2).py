fa59b14dd020990f4a5f4886f7b4a7221ddf859d
mibanezpin <mibanezpin@educacion.navarra.es>
2024-11-21 20:05:56 +0100

    ejercicio 2

---
+++ ejercicio 2 (2).py
@@ -0,0 +1,7 @@
+
+nombre_apellido= input("Escribe tu nombre en formato Nombre Apellido1 Apellido2")
+print(nombre_apellido.upper())
+print(nombre_apellido.lower())
+print(nombre_apellido.tittle())
+print("/n")
+
